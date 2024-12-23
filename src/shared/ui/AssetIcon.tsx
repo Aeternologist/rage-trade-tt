206db6b64a1208b6ce6403d2ad8f20ffc9d275e2
aeternologist <aeternologist@proton.me>
2024-12-23 14:16:09 +0500

    fix: :bug: network icon

--- src/shared/ui/AssetIcon.tsx
+++ src/shared/ui/AssetIcon.tsx
@@ -16,20 +16,22 @@ export const AssetIcon = ({
     const TokenIcon = TOKEN_ICON_BY_SYM[symbol];
     const NetworkIcon = NETWORK_ICON_BY_ID[chainId];
 
-    return TokenIcon ? (
+    return (
         <div className={cn('relative w-7', className)}>
-            <TokenIcon className={cn('rounded-full', className)} />
+            {TokenIcon ? (
+                <TokenIcon className={cn('rounded-full', className)} />
+            ) : (
+                <TextCircle
+                    className={cn(className)}
+                    size={24}
+                    iconStyle="BoldDuotone"
+                />
+            )}
             <NetworkIcon
-                className="absolute rounded-full right-0 bottom-0"
+                className="absolute bottom-0 right-0 rounded-full"
                 width={12}
                 height={12}
             />
         </div>
-    ) : (
-        <TextCircle
-            className={cn(className)}
-            size={24}
-            iconStyle="BoldDuotone"
-        />
     );
 };
