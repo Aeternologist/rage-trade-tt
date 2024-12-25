93baf923127259c86d535949f9e21ecb0c976218
aeternologist <aeternologist@proton.me>
2024-12-25 13:56:09 +0500

    fix: :bug: remove localforage from ssr

--- src/app/localforage.config.ts
+++ src/app/localforage.config.ts
@@ -2,12 +2,14 @@
 
 import localforage from 'localforage';
 
-localforage.config({
-    driver: localforage.INDEXEDDB,
-    name: `${process.env.NEXT_PUBLIC_APP_NAME}`,
-    storeName: `${process.env.NEXT_PUBLIC_APP_NAME} Data`,
-    description: `Store ${process.env.NEXT_PUBLIC_APP_NAME} Data`,
-    version: Number(process.env.NEXT_PUBLIC_LOCALFORAGE_STORE_VERSION),
-});
+if (typeof window !== 'undefined') {
+    localforage.config({
+        driver: localforage.INDEXEDDB,
+        name: `${process.env.NEXT_PUBLIC_APP_NAME}`,
+        storeName: `${process.env.NEXT_PUBLIC_APP_NAME} Data`,
+        description: `Store ${process.env.NEXT_PUBLIC_APP_NAME} Data`,
+        version: Number(process.env.NEXT_PUBLIC_LOCALFORAGE_STORE_VERSION),
+    });
+}
 
 export default localforage;
--- src/entities/TokenDetails/model/localStore.ts
+++ src/entities/TokenDetails/model/localStore.ts
@@ -1,10 +1,10 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import { z } from 'zod';
 import type { SupportedChainsId } from '@/shared/constants/supportedTokens';
 import { LocalForageStore } from '@/shared/lib/localforage';
 import type { Address } from '@/shared/lib/zod';
-
 
 const TokenMetadata = z.object({
     decimals: z.number().nullable(),
@@ -26,8 +26,10 @@ const TokensMetadataLocalStore = z.object({
 });
 export type TokensMetadataLocalStore = z.infer<typeof TokensMetadataLocalStore>;
 
-export const tokensMetadataLocalStore = new LocalForageStore({
-    name: 'Tokens Metadata',
-    description: 'Store metadata for Tokens',
-    schema: TokensMetadataLocalStore,
-});
+export const tokensMetadataLocalStore =
+    typeof window !== 'undefined' &&
+    new LocalForageStore({
+        name: 'Tokens Metadata',
+        description: 'Store metadata for Tokens',
+        schema: TokensMetadataLocalStore,
+    });
